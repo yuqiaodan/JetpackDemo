b373252810f3e7b40a09cae0748969574f0d1c7a
yuqiaodan <yuqiaodan@testbird.com>
2023-02-23 18:01:59 +0800

    fix bug

--- app/src/main/java/com/qiaodan/jetpackdemo/demo3_databinding/binddemo/StudentActivity.kt
+++ app/src/main/java/com/qiaodan/jetpackdemo/demo3_databinding/binddemo/StudentActivity.kt
@@ -8,19 +8,20 @@ import com.qiaodan.jetpackdemo.databinding.ActivityStudentBinding
 
 class StudentActivity : AppCompatActivity() {
 
-    private val binding by lazy {
-        //获取Binding方式1：inflate方法 需要设置 setContentView(binding.root)
-        ActivityStudentBinding.inflate(layoutInflater)
-        //获取Binding方式2：DataBindingUtil.setContentView 不需要设置 setContentView(binding.root) 直接用于activity
-        //DataBindingUtil.setContentView<ActivityStudentBinding>(this, R.layout.activity_student)
-
-        //获取Binding方式3：DataBindingUtil.inflate 需要设置 setContentView(binding.root) 一般用于fragment和adapter
-        //DataBindingUtil.inflate<ActivityStudentBinding>(layoutInflater, R.layout.activity_student,null,false)
-    }
+    private lateinit var binding: ActivityStudentBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+
+        //获取Binding方式1（推荐）：DataBindingUtil.setContentView 不需要设置 setContentView(binding.root) 直接用于activity
+        binding = DataBindingUtil.setContentView<ActivityStudentBinding>(this, R.layout.activity_student)
+
+        //获取Binding方式2（推荐）：DataBindingUtil.inflate 需要设置 setContentView(binding.root) 一般用于fragment和adapter
+        //binding = DataBindingUtil.inflate<ActivityStudentBinding>(layoutInflater, R.layout.activity_student,null,false)
+
+        //获取Binding方式3：inflate方法 需要设置 setContentView(binding.root)
+        //binding = ActivityStudentBinding.inflate(layoutInflater)
+
         binding.user = User("张三", 18, User.Gender.MAN)
 
     }
